6202dff146b8637ef928113096d1bddbef0e467b
AlexandrTil <alex3t33@gmail.com>
2020-11-01 18:46:14 +0200

    Code cleaning

--- app/src/main/java/com/example/gymtrainer/MainActivity.java
+++ app/src/main/java/com/example/gymtrainer/MainActivity.java
@@ -30,33 +30,8 @@ public class MainActivity extends AppCompatActivity implements DataAdapter.onIte
         LinearLayoutManager linearLayout = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayout);
         adapter = new DataAdapter(this, gyms, mListener);
-//myList(recyclerView);
         adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
-
- /*     switch (recyclerView.getId()){
-            case 0:
-                onItemClick(0);
-                break;
-            case 1:
-                onItemClick(1);
-                break;
-            case 2:
-                onItemClick(2);
-                break;
-            case 3:
-                onItemClick(3);
-                break;
-            case 4:
-                onItemClick(4);
-                break;
-            case 5:
-                onItemClick(5);
-                break;
-            default: break;
-
-        }*/
-
     }
 
     private void init() {
@@ -69,60 +44,10 @@ public class MainActivity extends AppCompatActivity implements DataAdapter.onIte
         Log.d("MyLog", "muscGroups.size(): " + gyms.size());
     }
 
-
-/*   private void myList(View view) {
-        switch (view.getId()) {
-            case 0:
-                onItemClick(view,0);
-                Log.d("MyLog", "pos: ");
-                break;
-            case 1:
-                onItemClick(view,1);
-                break;
-            case 2:
-                onItemClick(view,2);
-                break;
-            case 3:
-                onItemClick(view,3);
-                break;
-            case 4:
-                onItemClick(view,4);
-                break;
-            case 5:
-                onItemClick(view,5);
-                break;
-            default:
-                break;
-
-        }
-    }*/
-
     @Override
     public void onItemClick(View view, int pos) {
         Intent intent = new Intent(this, ExercisesActivity.class);
-       /* switch (pos) {
-            case 0:
-                intent.putExtra(Gym.POSITION, pos);
-                break;
-            case 1:
-                intent.putExtra(Gym.POSITION, pos);
-                break;
-            case 2:
-                intent.putExtra(Gym.POSITION, pos);
-                break;
-            case 3:
-                intent.putExtra(Gym.POSITION, pos);
-                break;
-            case 4:
-                intent.putExtra(Gym.POSITION, pos);
-                break;
-            case 5:
-                intent.putExtra(Gym.POSITION, pos);
-                break;
-            default:
-                break;
-        }*/
-          intent.putExtra(Gym.POSITION, pos);
+        intent.putExtra(Gym.POSITION, pos);
         startActivity(intent);
     }
 }
